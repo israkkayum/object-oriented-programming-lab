1a1d683f6dae79fe8783f90d140f84ed9254af7d
Israk Kayum <israkkayumchowdhury@israks-Air.netis.cc>
2022-06-22 12:21:19 +0600

    update

--- OOP_046_V.0.1/src/main/java/com/mycompany/calculator/Calculator.java
+++ OOP_046_V.0.1/src/main/java/com/mycompany/calculator/Calculator.java
@@ -10,110 +10,141 @@ public class Calculator {
 
         CalOperation cal = new CalOperation();
 
-        System.out.println("--------------------------- Calculator -----------------------------");
+        while (true) {
 
-        System.out.println("");
+            System.out.println("");
 
-        System.out.println("1. Sum\t\t 2. Sub\t\t 3. Div\t\t 4. Multiply\t 5. Modulus");
-        
-        System.out.println("");
-        
-        System.out.println("6. Root\t\t 7. x!\t\t 8. ln()\t 9. log()\t 10. sinx");
+            System.out.println("------------------------------- Calculator --------------------------------");
 
-        System.out.println("");
-        
-        System.out.println("11. cosx\t 12. tanx\t 13. scex\t 14. cosecx\t 15. cotx");
+            System.out.println("");
 
-        System.out.println("");
+            System.out.println("1. Sum\t\t 2. Sub\t\t 3. Div\t\t 4. Multiply\t 5. Modulus");
 
-        System.out.print("Choice your suitable option : ");
+            System.out.println("");
 
-        int n = s.nextInt();
+            System.out.println("6. Root\t\t 7. x!\t\t 8. ln()\t 9. log()\t 10. sinx");
 
-        System.out.print("Enter total number of input : ");
+            System.out.println("");
 
-        int size = s.nextInt();
+            System.out.println("11. cosx\t 12. tanx\t 13. scex\t 14. cosecx\t 15. cotx");
 
-        if (size == 1) {
+            System.out.println("");
 
-            System.out.print("Enter any one number : ");
+            System.out.println("16. Sin-1 x\t 17. cox-1 x\t 18. tan-1 x\t 19. exp(x)\t 20. log10");
 
-            cal.x = s.nextDouble();
+            System.out.println("");
 
-        } else if (size == 2) {
+            System.out.println("21. pow(x, y)\t 22. x-1\t 23. Exit\t");
 
-            System.out.print("Enter any two number : ");
+            System.out.println("");
 
-            cal.x = s.nextDouble();
-            cal.y = s.nextDouble();
+            System.out.print("Choice your suitable option : ");
 
-        } else {
-            System.out.println("Sorry, you can input maximum two number !!!");
-        }
+            int n = s.nextInt();
 
-        switch (n) {
-            case 1 -> {
-                cal.sum();
-                break;
+            while (n != 23) {
+
+                System.out.print("Enter total number of input : ");
+
+                int size = s.nextInt();
+
+                if (size == 1) {
+
+                    System.out.print("Enter any one number : ");
+
+                    cal.x = s.nextDouble();
+
+                } else if (size == 2) {
+
+                    System.out.print("Enter any two number : ");
+
+                    cal.x = s.nextDouble();
+                    cal.y = s.nextDouble();
+
+                } else {
+                    System.out.println("Sorry, you can input maximum two number !!!");
+                }
             }
-            case 2 -> {
 
-                cal.sub();
-                break;
+            switch (n) {
+                case 1 -> {
+                    cal.sum();
+                }
+                case 2 -> {
+
+                    cal.sub();
+                }
+                case 3 -> {
+                    cal.div();
+                }
+                case 4 -> {
+                    cal.multi();
+                }
+                case 5 -> {
+                    cal.modu();
+                }
+                case 6 -> {
+                    cal.root();
+                }
+                case 7 -> {
+                    cal.fact();
+                }
+                case 8 -> {
+                    cal.ln();
+                }
+                case 9 -> {
+                    cal.log();
+                }
+                case 10 -> {
+                    cal.sin();
+                }
+                case 11 -> {
+                    cal.cos();
+                }
+                case 12 -> {
+                    cal.tan();
+                }
+                case 13 -> {
+                    cal.sec();
+                }
+                case 14 -> {
+                    cal.cosec();
+                }
+                case 15 -> {
+                    cal.cot();
+                }
+                case 16 -> {
+                    cal.sinin();
+                }
+                case 17 -> {
+                    cal.cosin();
+                }
+                case 18 -> {
+                    cal.tanin();
+                }
+                case 19 -> {
+                    cal.exp();
+                }
+                case 20 -> {
+                    cal.log10();
+                }
+                case 21 -> {
+                    cal.pow();
+                }
+                case 22 -> {
+                    cal.xin();
+                }
+                case 23 -> {
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println("Enter valid Input!!");
+                    break;
+                }
             }
-            case 3 -> {
-                cal.div();
-                break;
-            }
-            case 4 -> {
-                cal.multi();
-                break;
-            }
-            case 5 -> {
-                cal.modu();
-                break;
-            }
-            case 6 -> {
-                cal.root();
-                break;
-            }
-            case 7 -> {
-                cal.fact();
-                break;
-            }
-            case 8 -> {
-                cal.ln();
-                break;
-            }
-            case 9 -> {
-                cal.log();
-                break;
-            }
-            case 10 -> {
-                cal.sin();
-                break;
-            }
-            case 11 -> {
-                cal.cos();
-                break;
-            }
-            case 12 -> {
-                cal.tan();
-                break;
-            }
-            case 13 -> {
-                cal.sec();
-                break;
-            }
-            case 14 -> {
-                cal.cosec();
-                break;
-            }
-            case 15 -> {
-                cal.cot();
-                break;
-            }
+
         }
 
     }
+
 }
